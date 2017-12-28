59540740ab2d3c0a5e4f8bf0a278a53c0d65ea7b
Petr <32482054+Petrychho@users.noreply.github.com>
2017-12-28 20:07:51 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+
 void join(const std::vector< int > &from, std::vector< int > &to)
 {
 	std::vector<int> out;
@@ -9,3 +15,56 @@ void join(const std::vector< int > &from, std::vector< int > &to)
 	
 	to = out;
 };
+
+void join1(const std::vector< int > &from, std::vector< int > &to)
+{
+	std::vector<int>::const_iterator first1, last1;
+	std::vector<int>::iterator first2, last2, tmp_iter;
+	std::vector<int> tmp;
+
+	tmp.reserve(from.size() + to.size());
+	first1 = from.begin();	last1 = from.end();
+	first2 = to.begin();	last2 = to.end();
+	tmp_iter = tmp.begin();
+	
+
+	while (first1 != last1 && first2 != last2)
+	{
+		tmp.push_back((*first1 < *first2) ? *first1++ : *first2++);
+	}
+	while (first1 != last1)
+	{
+		tmp.push_back(*first1++);
+	}
+	while (first2 != last2)
+	{
+		tmp.push_back(*first2++);
+	}
+	to = tmp;
+};
+	
+
+
+int main()
+{
+	std::vector<int> A{ 1,2,3,4,5,6,7,8,9 };
+	std::vector<int> B{ 4,5,6,7,8,9 };
+	std::vector<int> C{ 1,2,3,4,5,6,7,8,9 };
+	std::vector<int> D{ 4,5,6,7,8,9 };
+
+	join(A, B);	
+
+	std::cout << "B : ";
+	std::copy(B.begin(), B.end(),
+		std::ostream_iterator<int>(std::cout, " "));
+	std::cout << '\n';
+
+	join1(C, D);
+	
+	std::cout << "C : ";
+	std::copy(B.begin(), B.end(),
+		std::ostream_iterator<int>(std::cout, " "));
+	std::cout << '\n';
+
+	return 0;
+}
